29e6442cb5b81d85fdfde795a726ba942dec02b6
PermaNulled <crich@thedefaced.org>
2019-04-17 23:19:05 -0700

    Updated hook.ini removed current configurations and forced launcher to update it to fix some of the input issues game has and force windowed mode, options will be available later to re-size the window or force full-screen with the same input fixes used.

--- T4Browser/Player_Options.cs
+++ T4Browser/Player_Options.cs
@@ -85,14 +85,20 @@ namespace T4Browser
                 DialogResult dialogResult = MessageBox.Show("The Turok:Evolution MP mod was not found to be installed!\r\n\r\nWould you like the browser to install it?", "Warning - MOD NOT FOUND!", MessageBoxButtons.YesNo);
                 if (dialogResult == DialogResult.Yes)
                 {
-                    File.Copy("Mods/t4mp.dll", directory + "/t4mp.dll",true);
-                    File.Copy("Mods/T4MP.exe", directory + "/T4MP.exe",true); // This is a modified exe which will load t4mp.dll, this way we don't overwrite the original.
-                    File.Copy("Mods/d3d9.dll", directory + "/d3d9.dll", true); // have to be careful with this one, though it mostly fixes bugs.
-                    File.Copy("Mods/D3dHook.dll", directory + "/D3dHook.dll", true);
-                    File.Copy("Mods/hook.ini", directory + "/hook.ini", true); // This will have to be modified later to have the appropriate path.
-                    Directory.CreateDirectory(directory + "/T4MP");
-                    File.Copy("Mods/multiplayerjoin.ati", directory + "T4MP/multiplayerjoin.ati",true);
-                    File.Copy("Mods/mutliplayerjoin.txm", directory + "data/text/mutliplayerjoin.txm", true);
+                    File.Copy("Mods/t4mp.dll", directory + "\\t4mp.dll",true);
+                    File.Copy("Mods/T4MP.exe", directory + "\\T4MP.exe",true); // This is a modified exe which will load t4mp.dll, this way we don't overwrite the original.
+                    File.Copy("Mods/d3d9.dll", directory + "\\d3d9.dll", true); // have to be careful with this one, though it mostly fixes bugs.
+                    File.Copy("Mods/D3dHook.dll", directory + "\\D3dHook.dll", true);
+                    File.Copy("Mods/hook.ini", directory + "\\hook.ini", true); // This will have to be modified later to have the appropriate path.
+                    Directory.CreateDirectory(directory + "\\T4MP");
+                    File.Copy("Mods/multiplayerjoin.txm", directory + "\\data\\text\\multiplayerjoin.txm", true);
+                    File.Copy("Mods/multiplayerjoin.ati", directory + "\\T4MP\\multiplayerjoin.ati",true);
+
+                    // We'll also backup the history files we overwrite with the launcher.
+                    File.Copy(directory + "\\data\\history\\multiplayer.hst", directory + "\\data\\history\\multiplayer.hst.bak",true);
+                    File.Copy(directory + "\\data\\history\\multiadvancedoptions.hst", directory + "\\data\\history\\multiadvancedoptions.hst.bak",true);
+                    File.Copy(directory + "\\data\\history\\multiarsenal.hst", directory + "\\data\\history\\multiarsenal.hst.bak",true);
+
                 }
                 else if (dialogResult == DialogResult.No)
                 {
@@ -113,8 +119,19 @@ namespace T4Browser
             var parser = new FileIniDataParser();
             IniData data = new IniData();
             data["browser"]["game_path"] = browse.GamePath;
+            data["browser"]["game_dir"] = Path.GetDirectoryName(browse.GamePath);
             data["browser"]["player_name"] = player_name.Text;
             parser.WriteFile("T4Browser.ini", data);
+
+       
+            var hook_parser = new FileIniDataParser();
+            hook_parser.Parser.Configuration.AssigmentSpacer = "";
+            IniData hook_data = parser.ReadFile(Path.GetDirectoryName(browse.GamePath) + "\\hook.ini");
+            hook_data.Configuration.AssigmentSpacer = "";
+            hook_data["KnownProgram"][Path.GetDirectoryName(browse.GamePath) + "\\T4MP.exe"] = "Turok4";
+            hook_data["Prog.Turok4"]["SubModule0"] = Path.GetDirectoryName(browse.GamePath) + "\\D3DHook.dll";
+            hook_parser.WriteFile(Path.GetDirectoryName(browse.GamePath) + "\\hook.ini", hook_data, Encoding.ASCII);
+
 
             browse.PlayerName = player_name.Text;
 
